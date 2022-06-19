4657bb9d10e2713f9f8ab417ea27a97ed6d6cc93
Leonit <93375344+LeonitRe@users.noreply.github.com>
2022-06-19 21:43:27 +0200

    TrainerRegister-CRUD

--- App.js
+++ App.js
@@ -1,30 +1,154 @@
-import './App.css';
-import Create from './components/create/create';
-import Read from './components/read/read';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Update from './components/update/update';
+import React, { useState } from "react";
+import Constants from "./utilities/Constants";
+import PostCreateForm from "./components/PostCreateForm";
+import PostUpdateForm from "./components/PostUpdateForm";
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [showingCreateNewPostForm, setShowingCreateNewPostForm] = useState(false);
+  const [postCurrentlyBeingUpdated, setPostCurrentlyBeingUpdated] = useState(null);
+
+  function getPosts() {
+    const url = Constants.API_URL_GET_ALL_POSTS;
+
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(postsFromServer => {
+        setPosts(postsFromServer);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
+  function deletePost(postId) {
+    const url = `${Constants.API_URL_DELETE_POST_BY_ID}/${postId}`;
+
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(responseFromServer => {
+        console.log(responseFromServer);
+        onPostDeleted(postId);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
   return (
-    <Router>
-      <div className="main">
-        <div>
-          <h3>React Crud Operations</h3>
-        </div>
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          {(showingCreateNewPostForm === false && postCurrentlyBeingUpdated === null) && (
+            <div>
+              <h1>ASP.NET Core React Tutorial</h1>
 
-        <div>
-          <Route exact path='/' component={Create} />
-        </div>
+              <div className="mt-5">
+                <button onClick={getPosts} className="btn btn-dark btn-lg w-100">Get Posts from server</button>
+                <button onClick={() => setShowingCreateNewPostForm(true)} className="btn btn-secondary btn-lg w-100 mt-4">Create New Post</button>
+              </div>
+            </div>
+          )}
 
-        <div style={{ marginTop: 20 }}>
-          <Route exact path='/read' component={Read} />
-        </div>
+          {(posts.length > 0 && showingCreateNewPostForm === false && postCurrentlyBeingUpdated === null) && renderPostsTable()}
 
-        <Route path='/update' component={Update} />
-        
+          {showingCreateNewPostForm && <PostCreateForm onPostCreated={onPostCreated} />}
+
+          {postCurrentlyBeingUpdated !== null && <PostUpdateForm post={postCurrentlyBeingUpdated} onPostUpdated={onPostUpdated} />}
+        </div>
       </div>
-    </Router>
+    </div>
   );
-}
 
-export default App;
+  function renderPostsTable() {
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (PK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.postId}>
+                <th scope="row">{post.postId}</th>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>
+                  <button onClick={() => setPostCurrentlyBeingUpdated(post)} className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                  <button onClick={() => { if(window.confirm(`Are you sure you want to delete the post titled "${post.title}"?`)) deletePost(post.postId) }} className="btn btn-secondary btn-lg">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Empty React posts array</button>
+      </div>
+    );
+  }
+
+  function onPostCreated(createdPost) {
+    setShowingCreateNewPostForm(false);
+
+    if (createdPost === null) {
+      return;
+    }
+
+    alert(`Post successfully created. After clicking OK, your new post tilted "${createdPost.title}" will show up in the table below.`);
+
+    getPosts();
+  }
+
+  function onPostUpdated(updatedPost) {
+    setPostCurrentlyBeingUpdated(null);
+
+    if (updatedPost === null) {
+      return;
+    }
+
+    let postsCopy = [...posts];
+
+    const index = postsCopy.findIndex((postsCopyPost, currentIndex) => {
+      if (postsCopyPost.postId === updatedPost.postId) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      postsCopy[index] = updatedPost;
+    }
+
+    setPosts(postsCopy);
+
+    alert(`Post successfully updated. After clicking OK, look for the post with the title "${updatedPost.title}" in the table below to see the updates.`);
+  }
+
+  function onPostDeleted(deletedPostPostId) {
+    let postsCopy = [...posts];
+
+    const index = postsCopy.findIndex((postsCopyPost, currentIndex) => {
+      if (postsCopyPost.postId === deletedPostPostId) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      postsCopy.splice(index, 1);
+    }
+
+    setPosts(postsCopy);
+
+    alert('Post successfully deleted. After clicking OK, look at the table below to see your post disappear.');
+  }
+}
